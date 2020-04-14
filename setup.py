168f9faf052e57c63b560d58fc56482843b8e21c
hello-d4n <61355250+hello-d4n@users.noreply.github.com>
2020-04-14 11:16:56 +0200

    Setup script (cx_freeze)

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import sys
+from cx_Freeze import setup, Executable
+
+base = None
+if sys.platform == 'win32':
+    base = 'Win32GUI'
+
+executables = [
+    Executable(
+        script='import_pdf_into_styluslab.py',
+        icon='icon.ico',
+        base=base
+    )
+]
+includefiles = ['icon.ico']
+
+setup(name='Import PDF into StylusLab Write',
+      version='1.0',
+      author = "A. Daniel",
+      description='Convert PDF documents to HTML/SVG',
+      executables=executables,
+      options = {'build_exe': {'include_files':includefiles}}
+      )
