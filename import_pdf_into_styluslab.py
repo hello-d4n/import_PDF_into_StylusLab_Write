7ab560663d8c395df41df4ddc41805244db7be85
hello-d4n <61355250+hello-d4n@users.noreply.github.com>
2020-04-14 19:25:33 +0200

    Update import_pdf_into_styluslab.py

--- import_pdf_into_styluslab.py
+++ import_pdf_into_styluslab.py
@@ -14,7 +14,9 @@ if not is_gs_installed():
 
 ABS_PATH_CONVERT = get_abs_path_to_convert_exe()
 if ' ' in ABS_PATH_CONVERT:
-    ABS_PATH_CONVERT = '"' + ABS_PATH_CONVERT + '"'
+    ABS_PATH_CONVERT = add_quotes(ABS_PATH_CONVERT)
+    ABS_PATH_CONVERT = mod.double_backslash(ABS_PATH_CONVERT)
+    print(ABS_PATH_CONVERT)
 
 if ABS_PATH_CONVERT == None:
     myApp.display_get_convert_manually()
