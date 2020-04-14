633ac61e0a2ed25cb6cf30fc709c18a48fedf08f
hello-d4n <61355250+hello-d4n@users.noreply.github.com>
2020-04-14 11:13:01 +0200

    Update import_pdf_for_styluslab.py

--- import_pdf_for_styluslab.py
+++ import_pdf_for_styluslab.py
@@ -1,17 +1,22 @@
+# -*- coding: utf-8 -*-
+
 import my_module.module as mod
 import tkinter as tk
-import os
-
-ABS_PATH_CONVERT = mod.get_abs_path_to_convert_exe()
-# print(os.path.dirname(os.path.realpath(__file__)))
 
 window = tk.Tk()
 myApp = mod.MainWindow(window)
-myApp.set_title("Convert PDF for StylusLab")
+myApp.set_icon("icon.ico")
+
+if not mod.is_gs_installed():
+    myApp.window.withdraw()
+    myApp.display_gs_not_found_error()
+
+myApp.display_title()
+
+ABS_PATH_CONVERT = mod.get_abs_path_to_convert_exe()
 
 if ABS_PATH_CONVERT == None:
     myApp.display_get_convert_manually()
-    ABS_PATH_CONVERT = myApp.get_AbsPathConvertExe()
 else:
     myApp.set_abs_path_to_convert_exe(ABS_PATH_CONVERT)
 
